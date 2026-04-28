ac80e3ee6a79b50f88eca7feca94da865359b429
Jamil Uribe <jamiluribetech@gmail.com>
2026-04-28 02:25:25 -0300

    feat: Agrega carga del rol del usuario y mejora el diseño del menú

--- frontend/js/sesion.js
+++ frontend/js/sesion.js
@@ -1,10 +1,17 @@
 const usuario = JSON.parse(sessionStorage.getItem("usuarioLogueado"));
 
-//mostrar nombre
+// Mostrar nombre y rol
 if (!usuario) {
     window.location.href = "./index.html";
 } else {
+    // Carga el nombre del usuario
     document.getElementById("nombreUsuario").textContent = usuario.nombre;
+    
+    // Carga el rol del usuario si el elemento existe en la página
+    const elRol = document.getElementById("rolUsuario");
+    if (elRol) {
+        elRol.textContent = usuario.rol;
+    }
 }
 
 //cierre de sesion
@@ -33,9 +40,11 @@ if (divBotones) {
 }
 
 
-//Mostrar card para ir a historial de registros
+// Localizar la card de historial
 const cardHistorial = document.getElementById("cardHistorial");
+
 if (cardHistorial) {
+    // Si el usuario es supervisor, se muestra; de lo contrario, se elimina del diseño
     if (usuario.rol === "supervisor") {
         cardHistorial.classList.remove("d-none");
     } else {
